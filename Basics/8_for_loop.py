e88cd0bf9e4005c770f5bf9c911074c0a170ddd1
Shivam Tyagi <tyagishivamkk512@gmail.com>
2026-04-13 16:26:05 +0530

    Basic syntax of for loop

---
+++ Basics/8_for_loop.py
@@ -0,0 +1,11 @@
+for i in range(0,11): #will print from 0 to 10 
+    print(i)
+
+for i in range(0,11,2): # i will increase by 2 in every iteration 
+    print(i)
+
+# to iterate from 10 to 1: 
+for i in range(10,0,-1):
+    print(i)
+
+#or for i in reversed(range(0,11)):
