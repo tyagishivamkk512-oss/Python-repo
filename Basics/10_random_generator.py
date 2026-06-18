ae38ac0a2f9bfefd9dd7577e3d0b3d7970b21e73
Shivam Tyagi <tyagishivamkk512@gmail.com>
2026-06-18 17:20:50 +0530

    some more features of random module

--- Basics/10_random_generator.py
+++ Basics/10_random_generator.py
@@ -6,3 +6,7 @@ number = random.random() # generates random float number
 option = {'a','b','c','d'}
 random.choices(option) # will select random from option
 random.shuffle(option) #will shuffle options
+
+# random.choice will select one
+# random.choices(list, k=4) will select 4 
+# random.sample(list, 4) will select 4 
